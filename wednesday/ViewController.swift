0025b428b921ba7cd9e5cca9406cea22b2bcaf4d
dhillon1 <simrandhillon5486@gmail.com>
2019-11-20 18:55:20 -0500

    Initial Commit

---
+++ wednesday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wednesday
+//
+//  Created by Simran on 2019-11-20.
+//  Copyright © 2019 centennialcollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
